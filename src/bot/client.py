971ecb4bdc5df44a0ad3934205f48cc3bd15f23a
Kacper <109245559+kacpi-k@users.noreply.github.com>
2024-11-09 19:15:30 +0100

    Added !wait command

--- src/bot/client.py
+++ src/bot/client.py
@@ -1,9 +1,11 @@
 import discord
-from .commands import handle_message
+from .commands import handle_message, start_timer
+from .config import ALLOWED_CHANNELS_ID
 
 intents = discord.Intents.default()
 intents.message_content = True
 discordClient = discord.Client(intents=intents)
+
 
 @discordClient.event
 async def on_ready():
@@ -11,4 +13,15 @@ async def on_ready():
 
 @discordClient.event
 async def on_message(message):
-    await handle_message(discordClient, message)
+    if message.channel.id not in ALLOWED_CHANNELS_ID:
+        print("Zły kanał")
+        return
+
+    if message.author == discordClient.user:
+        return
+
+    if message.content.startswith('!pytaj'):
+        await handle_message(message)
+
+    if message.content.startswith('!czekaj'):
+        await start_timer(message)
